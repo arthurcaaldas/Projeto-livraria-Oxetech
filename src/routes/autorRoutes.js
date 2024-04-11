e2d11a7287d60396526f0048310a842372ecd150
MonteiroAlbert <dev.albertmonteiro@gmail.com>
2024-04-11 12:33:28 -0300

    Correção das rotas em autorRoutes.js, livroRoutes.js e Povoamento do Banco de Dados

--- src/routes/autorRoutes.js
+++ src/routes/autorRoutes.js
@@ -7,7 +7,7 @@ const router = Router();
 
 router.get('/autores', (req, res) => autorController.getTodosRegistros(req,res));
 router.get('/autores/:id', (req, res) => autorController.getRegistroPorId(req,res));
-router.post('/autores', (req, res) => autorController.cria(req,res));
+router.post('/autores', (req, res) => autorController.create(req,res));
 router.put('/autores/:id', (req, res) => autorController.update(req,res));
 router.delete('/autores/:id', (req, res) => autorController.delete(req,res));
 
